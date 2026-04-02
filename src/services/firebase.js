f9ec7bc7e1abc501d850c210f15f0b15f28ded70
Nayan Soni <nayanmaheshwari898@example.com>
2026-04-02 23:07:01 +0530

    fix vercel json

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,4 +1,3 @@
-console.log("API KEY CHECK:", import.meta.env.VITE_API_KEY);
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
